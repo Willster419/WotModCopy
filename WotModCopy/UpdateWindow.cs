2cd8c74e9fe3253ac4532f4d2fc4ec33fe195ebc
Willster419 <willster419@outlook.com>
2016-10-20 11:54:55 -0400

    init program start

---
+++ WotModCopy/UpdateWindow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ReplayTemplate
+{
+    public partial class UpdateWindow : Form
+    {
+        public bool update = false;
+        public UpdateWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void noButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void yesButton_Click(object sender, EventArgs e)
+        {
+            update = true;
+            this.Close();
+        }
+    }
+}
